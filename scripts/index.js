70a689690ba6abd4202332d4dc6867dba52bf1c4
nocTKpunTyM <noctkpuntym@yandex.ru>
2023-01-24 18:39:50 +0300

    На 2 ревью. 22 ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,34 @@ let profileName = document.querySelector('.lead__title');
 let profileJob = document.querySelector('.lead__subtitle');
 let inputName = document.querySelector('.popup__form-input_type_name');
 let inputJob = document.querySelector('.popup__form-input_type_job');
+let editButton = document.querySelector('.lead__edit-button');
+let popupX = document.querySelector('.popup__close-button');
 
 function insertProfileToForm() {
     inputName.value = profileName.textContent;
     inputJob.value = profileJob.textContent;
 }
+
 function openPopup() {
     insertProfileToForm();
     popup.classList.add('popup_opened');
 }
-let editButton = document.querySelector('.lead__edit-button');
-editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
 
 function insertFormToProfile() {
     profileName.textContent = inputName.value;
     profileJob.textContent = inputJob.value;
 }
+
 function savePopup(event) {
     event.preventDefault();
     insertFormToProfile();
-    popup.classList.remove('popup_opened');
+    closePopup();
 }
-popupForm.addEventListener('submit', savePopup);
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
-let popupX = document.querySelector('.popup__close-button');
+editButton.addEventListener('click', openPopup);
 popupX.addEventListener('click', closePopup);
+popupForm.addEventListener('submit', savePopup);
